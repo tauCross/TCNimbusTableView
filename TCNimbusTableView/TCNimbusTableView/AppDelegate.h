7557a06ea1101aa3a77e346c8e6d219ea644420d
tauCross <mstsccls@gmail.com>
2017-04-17 10:23:16 +0800

    no message

---
+++ TCNimbusTableView/TCNimbusTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCNimbusTableView
+//
+//  Created by tauCross on 2017/4/17.
+//  Copyright © 2017年 Auto Financing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
